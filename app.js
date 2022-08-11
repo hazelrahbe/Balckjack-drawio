f9979445aae5a05548c028d8f9951d68a854d3aa
hazelrahbe <swp.hazel94@gmail.com>
2022-08-11 14:31:13 -0400

    Almost there 2

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ let hitBtn = document.querySelector('.hitBtn')
 let stayBtn = document.querySelector('.stayBtn')
 let playerCard = document.querySelector('.player')
 let dealerCard = document.querySelector('.dealer')
+let htmlWin = document.querySelector(".win")
 
 
 function startGame() {
@@ -53,41 +54,49 @@ function calculateTotal(hand) {
         else if(card.points === 'A') total += 11;
         else total += card.points;
     });
+    console.log(hand)
+    console.log(total)
     return total;
 }
 
 function hitMe() {
     playerHand.push(deck[0])
     deck.splice(0, 1)
-    if (calculateTotal(playerHand) >= 21) {
-        document.querySelector('.playerText').innerHTML = (playerHand)
-    alert("You lose")
+    appendCard(playerHand[0], playerCard)
+    document.querySelector('.playerText').innerHTML = calculateTotal(playerHand)
+    if (calculateTotal(playerHand) > 21) {
+        // document.querySelector('.playerText').innerHTML = (playerHand)
+        // document.querySelector('.dealerText').innerHTML = (dealerHand)
+        // document.querySelector('.dealerText').innerHTML = calculateTotal(dealerHand)
+        htmlWin.innerHTML = ("You lose")
     }
-    document.querySelector('.playerText').innerHTML = (calculateTotal(playerHand))
-    // if (dealerHand < 17) {
-    // dealerHand.push(deck[0])
-    // deck.splice(0, 1)
-    // dealerHand.value = dealerHand.value + deck[0]
+//     if (dealerHand < 17) {
+//     dealerHand.push(deck[0])
+//     deck.splice(0, 1)
+//     appendCard(dealerHand[0], dealerCard)
 
-    // console.log(dealerHand)
-    // console.log(dealerHand.value + " Points")
+//     document.querySelector('.dealerText').innerHTML = (dealerHand)
+//     document.querySelector('.dealerText').innerHTML = (calculateTotal(dealerHand))
+// }
 }
- 
 
 hitBtn.addEventListener('click', hitMe)
 
 function skipMe() {
-    if (dealerHand.value < 17) {
+    if (calculateTotal(dealerHand) < 17) {
     dealerHand.push(deck[0])
         deck.splice(0, 1)
-        document.querySelector('.playerText').innerHTML = (playerHand)
-        document.querySelector('.playerText').innerHTML = (dealerHand)
+        appendCard(dealerHand[0], dealerCard)
+        // document.querySelector('.playerText').innerHTML = (playerHand)
+        // document.querySelector('.playerText').innerHTML = (dealerHand)
         // (playerHand, dealerHand)
+        document.querySelector('.dealerText').innerHTML = calculateTotal(dealerHand)
+
     skipMe();
     }
-    else if (calculateTotal(dealerHand)) {
+    else if (calculateTotal(dealerHand) > 21) {
         console.log(dealerHand)
-        alert("You win! Dealer Lose");
+        htmlWin.innerHTML = "Dealer Win"
     }
     else {
         checkPoints()
@@ -97,13 +106,16 @@ function skipMe() {
 stayBtn.addEventListener('click', skipMe)
 
 function checkPoints() {
-    if ( calculateTotal(playerHand) < calculateTotal(dealerHand) || playerHand == 21) {
-        alert("You Lose! Dealer Wins!");
-        console.log(`Player hand: ${playerHand}, dealer hand: ${dealerHand}`)
+    if ( calculateTotal(playerHand) < calculateTotal(dealerHand)) {
+        htmlWin.innerHTML = "You Lose! Dealer Wins!";
+        // console.log(`Player hand: ${playerHand}, dealer hand: ${dealerHand}`)
+    }
+    else if (calculateTotal(playerHand) > calculateTotal(dealerHand)){
+        htmlWin.innerHTML = "You Win! Dealer Lose!";
+        // console.log(`Player hand: ${playerHand}, dealer hand: ${dealerHand}`)
     }
     else {
-        alert("You Win! Dealer Lose!");
-        dc(`Player hand: ${playerHand}, dealer hand: ${dealerHand}`)
+        htmlWin.innerHTML = "Its Draw"
     }
 }
 
@@ -115,3 +127,7 @@ function appendCard(card, turn) {
     img.style.width = '80px'
     img.style.height = '120px'
 }
+
+function reset(){  
+    document.querySelector('.newGame').reset();  
+  }
